458fd8612e8e8316d5d22f133acca02f4bdf4c6d
Danil Kurmashev <danilprofiru@gmail.com>
2025-03-26 23:13:17 +0300

    Add ChooseImageActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/mtuci_project_practicum_2025/ChooseImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.mtuci_project_practicum_2025;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ChooseImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_image);
+
+    }
+}
